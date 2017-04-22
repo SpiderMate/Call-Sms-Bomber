c406cbc5940061af3287daa29a7ed4c8ca12dc1b
Suvadip Kar <suvadip.tech@gmail.com>
2017-04-22 09:58:35 -0400

    Update SpidyBomber.sh

--- SpidyBomber.sh
+++ SpidyBomber.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+'
+# Call-Sms-Bomber
+Programmer: Suvadip Kar
+Website: Suvadipkar.com
+Facebook: http://facebook.com/suvadip.kar.96
+Twitter: https://twitter.com/KarSuvadip
+Bugcrowd: https://bugcrowd.com/Suvadipkar
 
+Rules:
+1. Dont use this tool to dos any anyone.
+2. Plz give credits when you modify it.
+
+Note: This tool is for educational purpose.
+'
 clear
 
 NC='\033[0m'          
